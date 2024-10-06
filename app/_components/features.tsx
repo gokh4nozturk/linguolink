f574135a88c4a338230001d159dfca286b15bce7
Gokhan Ozturk <gokhaanozturk@gmail.com>
2024-10-06 16:22:38 +0300

    Add new components

--- app/_components/features.tsx
+++ app/_components/features.tsx
@@ -1,14 +1,16 @@
+import Cards from "@/components/cards";
 import CTA from "@/components/cta";
 import Title from "@/components/title";
 
 export default function Features() {
 	return (
-		<section id="features" className="h-dvh scroll-m-24 text-center">
+		<section id="features" className="min-h-dvh scroll-m-24 text-center">
 			<Title>Features</Title>
 			<CTA
 				title="Why choose our service?"
 				description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 			/>
+			<Cards />
 		</section>
 	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -65,17 +65,35 @@ const config = {
 			},
 			keyframes: {
 				"accordion-down": {
-					from: { height: "0" },
-					to: { height: "var(--radix-accordion-content-height)" },
+					from: {
+						height: "0",
+					},
+					to: {
+						height: "var(--radix-accordion-content-height)",
+					},
 				},
 				"accordion-up": {
-					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: "0" },
+					from: {
+						height: "var(--radix-accordion-content-height)",
+					},
+					to: {
+						height: "0",
+					},
+				},
+				"background-position-spin": {
+					"0%": {
+						backgroundPosition: "top center",
+					},
+					"100%": {
+						backgroundPosition: "bottom center",
+					},
 				},
 			},
 			animation: {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
+				"background-position-spin":
+					"background-position-spin 3000ms infinite alternate",
 			},
 			fontFamily: {
 				sans: ["var(--font-sans)", ...fontFamily.sans],
