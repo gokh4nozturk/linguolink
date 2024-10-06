51ffd8ae42cd2d1beb106cf19fb21f8511cdc729
Gokhan Ozturk <gokhaanozturk@gmail.com>
2024-10-06 16:41:32 +0300

    Refactor Cards component to use new Card component and update card content and styling

--- components/cards.tsx
+++ components/cards.tsx
@@ -1,52 +1,63 @@
 "use client";
 
 import React from "react";
-
 import {
 	Card,
 	CardContent,
 	CardDescription,
-	CardFooter,
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
-import { Loader, X } from "lucide-react";
+import { ChartSpline, Check, Cog, HandCoins, Lock, Rabbit } from "lucide-react";
 
 export default function Cards() {
 	const cards = [
 		{
-			title: "Card Title",
-			description: "Card Description",
-			content: "Card Content",
-			footer: "Card Footer",
-			icon: <Loader />,
+			title: "Easy to use",
+			description:
+				"User-friendly interface designed to simplify complex tasks, making it easy for anyone to get started.",
+			icon: <Cog />,
 		},
 		{
-			title: "Card Title",
-			description: "Card Description",
-			content: "Card Content",
-			footer: "Card Footer",
-			icon: <X />,
+			title: "Reliable",
+			description:
+				"Built with stability in mind, ensuring consistent performance and minimizing downtime.",
+			icon: <Check />,
 		},
 		{
-			title: "Card Title",
-			description: "Card Description",
-			content: "Card Content",
-			footer: "Card Footer",
-			icon: <Loader />,
+			title: "Secure",
+			description:
+				"Advanced security measures to protect your data and ensure privacy.",
+			icon: <Lock />,
+		},
+		{
+			title: "Fast",
+			description:
+				"Optimized for speed to deliver quick responses and efficient processing.",
+			icon: <Rabbit />,
+		},
+		{
+			title: "Affordable",
+			description:
+				"Cost-effective solutions that provide high value without breaking the bank.",
+			icon: <HandCoins />,
+		},
+		{
+			title: "Scalable",
+			description:
+				"Designed to grow with your needs, easily handling increasing workloads and users.",
+			icon: <ChartSpline />,
 		},
 	];
 	return (
-		<div className="grid grid-cols-2 gap-4">
+		<div className="grid md:grid-cols-2 xl:grid-cols-3 gap-6">
 			{cards.map((card, index) => (
 				<Card key={index}>
 					<CardHeader>{card.icon}</CardHeader>
-					<CardContent>
+					<CardContent className="text-left space-y-1">
 						<CardTitle>{card.title}</CardTitle>
 						<CardDescription>{card.description}</CardDescription>
-						<p>{card.content}</p>
 					</CardContent>
-					<CardFooter>{card.footer}</CardFooter>
 				</Card>
 			))}
 		</div>
