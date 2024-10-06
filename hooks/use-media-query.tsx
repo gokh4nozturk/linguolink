7484e13fb181ddb876c469ced847095459eb13c6
Gokhan Ozturk <gokhaanozturk@gmail.com>
2024-10-06 17:50:02 +0300

    add mounted checker

--- hooks/use-media-query.tsx
+++ hooks/use-media-query.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 export default function useMediaQuery(query: string) {
-	if (typeof window === "undefined") return;
+	const [mounted, setMounted] = React.useState(false);
+
+	React.useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (!mounted) return false;
+
 	const mediaQuery = window.matchMedia(query);
 	const [isMatch, setIsMatch] = React.useState(mediaQuery.matches);
 
