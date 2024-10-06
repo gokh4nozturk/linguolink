d81a540dfc6eebd9127008dfa243a6f94d58624d
Gokhan Ozturk <gokhaanozturk@gmail.com>
2024-10-06 17:17:37 +0300

    Refactor banner and CTA components to update content and styling

--- app/_components/banner.tsx
+++ app/_components/banner.tsx
@@ -1,17 +1,19 @@
-import CTA from "@/components/cta";
 import { Button } from "@/components/ui/button";
 import { Link } from "next-view-transitions";
 
 export default function Banner() {
 	return (
 		<section id="banner" className="h-dvh scroll-m-24 text-center py-32">
-			<Button>
-				<Link href="/login">Login</Link>
-			</Button>
-			<CTA
-				title="Welcome to our website"
-				description="Proident sunt exercitation minim laborum enim laboris labore esse."
-			/>
+			<div className="text-foreground">
+				<div className="py-12 text-center">
+					<h1 className="text-3xl sm:text-7xl font-bold">
+						Welcome to our website
+					</h1>
+					<p className="text-lg mt-4 text-foreground/60">
+						Proident sunt exercitation minim laborum enim laboris labore esse.
+					</p>
+				</div>
+			</div>
 		</section>
 	);
 }
--- components/cta.tsx
+++ components/cta.tsx
@@ -6,8 +6,8 @@ interface CTAProps {
 
 export default function CTA({ title, description, children }: CTAProps) {
 	return (
-		<div className="bg-accent-1 text-foreground">
-			<div className="container mx-auto py-12 text-center">
+		<div className="text-foreground">
+			<div className="py-12 text-center">
 				<h2 className="text-3xl sm:text-5xl font-bold">{title}</h2>
 				<p className="text-lg mt-4 text-foreground/60">{description}</p>
 				<div className="mt-6">{children}</div>
