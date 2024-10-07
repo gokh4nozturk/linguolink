7b459686a493b940340e1f2ff6c063f497281b13
Gokhan Ozturk <gokhaanozturk@gmail.com>
2024-10-07 11:53:04 +0300

    Refactor footer component and add legal links

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,21 @@ export default function Footer() {
 	return (
 		<footer className="bg-soft-blue-foreground text-white text-center p-24">
 			<p className="text-sm">&copy; 2024. All rights reserved.</p>
+			<a
+				href="https://linguolink-s3.s3.eu-central-1.amazonaws.com/legal/LinguoLink-Privacy-Policy.pdf"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Privacy Policy
+			</a>
+			{" | "}
+			<a
+				href="https://linguolink-s3.s3.eu-central-1.amazonaws.com/legal/LinguoLink-Terms.pdf"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Terms
+			</a>
 		</footer>
 	);
 }
