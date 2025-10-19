68c2d2b138820cefad704b3f8a5052d1b15e2b09
gokhanozturk <gokhaanozturk@gmail.com>
2025-10-19 09:18:46 +0300

    Add top padding to main container in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import StructuredData from '@/components/structured-data';
 import type { Metadata } from 'next';
+import StructuredData from '@/components/structured-data';
 import Banner from './_components/banner';
 import FAQ from './_components/faq';
 import Features from './_components/features';
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main
-      className='grid min-h-screen grid-cols-1 gap-20 p-4 max-sm:gap-10 sm:p-24'
+      className='grid min-h-screen grid-cols-1 gap-20 p-4 pt-24 max-sm:gap-10 sm:p-24'
       itemScope
       itemType='https://schema.org/WebPage'
     >
